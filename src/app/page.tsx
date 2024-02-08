2c76142e539113e247b6d1b964cf10d71c5a0795
Alpha <piotr@piotryordanov.com>
2024-02-08 18:32:49 +0400

    fix: remove prettier

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as React from "react";
+
+export default function HomePage() {
+  return <div className="w-screen h-screen bg-primary"></div>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#292d39",
+      },
+      keyframes: {
+        flicker: {
+          "0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%": {
+            opacity: "0.99",
+            filter:
+              "drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))",
+          },
+          "20%, 21.999%, 63%, 63.999%, 65%, 69.999%": {
+            opacity: "0.4",
+            filter: "none",
+          },
+        },
+        shimmer: {
+          "0%": {
+            backgroundPosition: "-700px 0",
+          },
+          "100%": {
+            backgroundPosition: "700px 0",
+          },
+        },
+      },
+      animation: {
+        flicker: "flicker 3s linear infinite",
+        shimmer: "shimmer 1.3s linear infinite",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+} satisfies Config;
